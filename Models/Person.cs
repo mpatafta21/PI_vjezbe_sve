3325d25a7adb511f7bedf89717b618483060be1a
Maks Patafta <mpatafta21@foi.hr>
2023-05-20 19:45:03 +0200

    Implementacija hijerarhije

---
+++ Models/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_vjezbe_sve.Models
+{
+    public abstract class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+    public class Teacher : Person
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+    public class Student : Person
+    {
+        public int Grade { get; set; }
+    }
+
+}
