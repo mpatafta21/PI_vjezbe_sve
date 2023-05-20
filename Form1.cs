98de72864232766a1b52202793dd96d468d8e502
Maks Patafta <mpatafta21@foi.hr>
2023-05-20 19:30:14 +0200

    Dodana forma FrmStudents

--- Form1.cs
+++ Form1.cs
@@ -42,8 +42,10 @@ namespace PI_vjezbe_sve
             {
                 if (txtUsername.Text == username && txtPassword.Text == password)
                 {
-                    MessageBox.Show("Dobrodošli", "Prijavljeni ste",
-                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    FrmStudents frmStudents = new FrmStudents();
+                    Hide();
+                    frmStudents.ShowDialog();
+                    Close();
                 }
                 else
                 {
